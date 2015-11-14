523f4f5d797f890bdf9608ca8ee0ba15245078f5
章许帆 <sunshine@zhangxufans-MacBook-Pro.local>
2015-11-14 15:38:22 +0800

    add configuration file

---
+++ src/main/java/fishing/sunshine/controller/WechatController.java
@@ -0,0 +1,10 @@
+package fishing.sunshine.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sunshine on 11/14/15.
+ */
+@RestController
+public class WechatController {
+}
---
+++ src/main/java/fishing/sunshine/model/Configuration.java
@@ -0,0 +1,61 @@
+package fishing.sunshine.model;
+
+/**
+ * Created by sunshine on 11/14/15.
+ */
+public class Configuration {
+    private String jsapiTicket;
+    private String nonceStr;
+    private String timestamp;
+    private String signature;
+    private String url;
+    private String shareLink;
+
+    public String getJsapiTicket() {
+        return jsapiTicket;
+    }
+
+    public void setJsapiTicket(String jsapiTicket) {
+        this.jsapiTicket = jsapiTicket;
+    }
+
+    public String getNonceStr() {
+        return nonceStr;
+    }
+
+    public void setNonceStr(String nonceStr) {
+        this.nonceStr = nonceStr;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getShareLink() {
+        return shareLink;
+    }
+
+    public void setShareLink(String shareLink) {
+        this.shareLink = shareLink;
+    }
+}
