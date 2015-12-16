5cc94a588a2f60948b1fbbc919ecce2cb34b09d8
zhangxufan <sunshinezxf@hotmail.com>
2015-12-16 23:16:24 +0800

    tend to include wechat into the project

--- src/main/java/fishing/sunshine/controller/WechatController.java
+++ src/main/java/fishing/sunshine/controller/WechatController.java
@@ -1,10 +1,26 @@
 package fishing.sunshine.controller;
 
+import fishing.sunshine.util.CommonValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by sunshine on 11/14/15.
  */
 @RestController
 public class WechatController {
+    private Logger logger = LoggerFactory.getLogger(WechatController.class);
+
+    @RequestMapping(method = RequestMethod.GET, value = "/" + CommonValue.TOKEN)
+    @ResponseBody
+    public String check(HttpServletRequest request) {
+        
+        return "";
+    }
 }
---
+++ src/main/java/fishing/sunshine/util/CommonValue.java
@@ -0,0 +1,8 @@
+package fishing.sunshine.util;
+
+/**
+ * Created by sunshine on 12/16/15.
+ */
+public class CommonValue {
+    public static final String TOKEN = "wechat";
+}
